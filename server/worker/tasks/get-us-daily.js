135154259c9a2a4da551f9f2a1aa95f0f5cf6a4a
Duc Tran <ducvtrann@gmail.com>
2020-08-28 19:23:28 -0400

    Add corona task for us-daily covid numbers

---
+++ server/worker/tasks/get-us-daily.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const redis = require('redis');
+const client = redis.createClient();
+
+const { promisify } = require('util');
+const setAsync = promisify(client.set).bind(client);
+
+const baseURL = 'https://covidtracking.com/api/us/daily';
+
+async function getDailyUS() {
+  console.log('Retrieving the past 14 days of COVID data');
+
+  let pastTwoWeeks = [];
+  const { data } = await axios.get(baseURL);
+  pastTwoWeeks = [...data.slice(0, 14)];
+
+  await setAsync('us-daily', JSON.stringify(pastTwoWeeks));
+  console.log('Stored us daily into redis');
+}
+
+module.exports = fetchGithub;
