a0473fed94f6dbd613872778d888dbc057b4fcb6
Duc Tran <ducvtrann@gmail.com>
2020-08-30 20:24:19 -0400

    Add redis config to index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const express = require('express');
-const redis = require('redis');
+// const redis = require('redis');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+if (process.env.REDISTOGO_URL) {
+  const rtg = require('url').parse(process.env.REDISTOGO_URL);
+  const redis = require('redis').createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.auth.split(':')[1]);
+} else {
+  var redis = require('redis').createClient();
+}
 
 app.use(cors());
 
